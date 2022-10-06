0d147d5b459d7db317943ee5c7c343372217fe4e
Milan <milanbulatovic@outlook.com>
2022-10-06 16:42:37 +0200

    refactoring

--- app.js
+++ app.js
@@ -4,21 +4,19 @@ var app = angular.module('Edit', []);
 //register controller
 app.controller('MainController', [ '$scope', function($scope) {
 
-		$scope.value = 'Click to edit';
-		// $scope is a special object that makes
-		// its properties available to the view as
-		// variables.
-		//$scope.showtooltip = false;
+	// $scope is a special object that makes
+	// its properties available to the view as
+	// variables.
+	$scope.value = 'Click to edit';
 
-		// Some helper functions that will be
-		// available in the angular declarations
-		
+	//$scope.showtooltip = false
+	
 
-		//toggle tooltip visibility 
-		$scope.toggleTooltip = function (e) {
-			e.stopPropagation();
-			//showtooltip = oposite of what it is at the moment
-			//if true "!" will make it false and vice versa 
-			$scope.showtooltip = !$scope.showtooltip;
-		};
-	}]);
+	//toggle tooltip visibility 
+	$scope.toggleTooltip = function (e) {
+		e.stopPropagation();
+		//showtooltip = oposite of what it is at the moment
+		//if true "!" will make it false and vice versa 
+		$scope.showtooltip = !$scope.showtooltip;
+	};
+}]);
